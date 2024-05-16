a7cbb21c01028214c0245a67a94fa90b6f762b38
Vikash <vikashdas770@gmail.com>
2024-05-16 11:11:27 +0530

    final commit, readme updated

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# GenAI-Gemini-pdfInterpreter
-An AI application, powered by Gemini AI API that takes a pdf doc and prompt as an input, returns back the info from the doc as a response.
+# GenAI-Gemini-InvoiceInterpreter
+
+GenAI-Gemini-InvoiceInterpreter is an AI application powered by the Gemini AI API. It takes an invoice image document and a prompt as input and returns the information extracted from the image as a response.
+
+## Getting Started
+
+Follow these steps to set up the project:
+
+1. **Create GitHub Repository**: Create a new GitHub repository and clone it locally.
+
+2. **Obtain API Key**: Get your API key from [here](https://makersuite.google.com/app/apikey).
+
+3. **Set Up Virtual Environment**: Add a `requirements.txt` file with the necessary dependencies and create a virtual environment.
+
+    ```bash
+    conda create -p imgreader python==3.10 -y
+    conda activate imgreader
+    pip install -r requirements.txt
+    ```
+
+    Add the virtual environment directory (`imgreader/`) to `.gitignore` to exclude it from version control.
+
+4. **Create Environment File**: Create a `.env` file and add your API key as follows:
+
+    ```
+    API_KEY="XXXXXXXXXX"
+    ```
+
+5. **Setup Application**: Set up your `app.py` file and add code to it.
+
+## Usage
+
+After completing the setup steps, run your application. It should be able to take an invoice image document and a prompt as input and return the extracted information from the image as a response.
+
+## Contributors
+
+- [Vikash Das](https://github.com/vikashishere)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
---
+++ app.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import os
+from PIL import Image
+import google.generativeai as genai
+
+from dotenv import load_dotenv
+load_dotenv() # Load all env var from .env file
+
+# Configuring API key
+genai.configure(api_key = os.getenv("API_KEY"))
+
+# Loading Gemini Pro vision model to get response
+def get_gemini_response(input, image, prompt):
+    model = genai.GenerativeModel('gemini-pro-vision')
+    response = model.generate_content([input, image[0], prompt])
+    return response.text
+
+def input_image_setup(uploaded_file):
+    if uploaded_file is not None:
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type" : uploaded_file.type,
+                "data" : bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+    
+
+# Initializing Streamlit app
+st.set_page_config("Gemini Application - Invoice Extractor")
+input = st.text_input("Input Prompt: ", key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image = ""
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+submit = st.button("Tell me about the invoice")
+
+
+
+input_prompt = """You are an expert in understanding invoices. 
+You will receive invoice images as input and you need to extract 
+the info from image to answer any question."""
+
+
+if submit:
+    image_data = input_image_setup(uploaded_file)
+    response = get_gemini_response(input_prompt, image_data, input)
+
+    st.subheader("Here is the response for you...")
+    st.write(response)
